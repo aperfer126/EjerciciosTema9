33f237609c1f56380da2b0978c700b1a8f261c95
Alicia <apf737@gmail.com>
2025-03-19 14:29:41 +0100

    asda

--- ejerciciostema9/src/main/java/com/apf/PrimaryController.java
+++ ejerciciostema9/src/main/java/com/apf/PrimaryController.java
@@ -1,12 +1,35 @@
 package com.apf;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        loadDataConsole();
+    }
+
+    private void loadDataConsole() throws IOException{
+    Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/Usuarios", "root", "root");
+            System.out.println("La conexión ha sido realizada con exito");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Usuarios");
+            while (rs.next()){
+                System.out.println(rs.getString("id") + rs.getString("nick") + rs.getString("email"));
+            }
+            con.close();
+        }
+        catch(Exception e) {
+            System.out.println("Error de SQL: " + e.getMessage());
+        }
     }
 }
--- ejerciciostema9/src/main/java/module-info.java
+++ ejerciciostema9/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.apf {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
 
     opens com.apf to javafx.fxml;
     exports com.apf;
